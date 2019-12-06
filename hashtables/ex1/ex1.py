861b7d6e4a55ec2722a1bbc6a3ba515482d5478e
Philip Johnson <phil7j@gmail.com>
2019-12-06 12:03:59 -0800

    finished example 1 for hashtables

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,11 +9,24 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    # insert weights to hash table with index as value
+    for i, weight in enumerate(weights):
+        # print(i, weight)
+        hash_table_insert(ht, weight, i)
+    # check to see if hashtable contains a entry for limit - weight
+    for i, weight in enumerate(weights):
+        # index we are looking for will be the difference of limit and the current weight
+        index = limit - weight
+        # If we found one, capture the value and return as a tuple
+        if hash_table_retrieve(ht, index) is not None:
+            other_index = hash_table_retrieve(ht, index)
+            # return larger weight first in tuple
+            if other_index > i:
+                return (other_index, i)
+            else:
+                return (i, other_index)
+    # return hash_table_retrieve(ht, 6)
 
-    return None
 
 
 def print_answer(answer):
@@ -21,3 +34,8 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+
+weights_3 = [4, 6, 10, 15, 16]
+answer_3 = get_indices_of_item_weights(weights_3, 5, 21)
+print(answer_3)
